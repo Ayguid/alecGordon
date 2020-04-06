8b40ca74e20773616f2070a1086d790f7350ae47
Ayguid <guidoaimar@gmail.com>
2020-04-06 15:32:01 -0300

    14 cmt

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,26 +8,34 @@
       <img width="100%" src="{{asset('img/landing/logoLanding.png')}}" alt="">
     </div>
 
-
+    @php
+      // $video = asset('video/LANDING_2020.mp4');
+      $video = 'https://player.vimeo.com/video/'."395691905"."?background=1&autoplay=1&loop=1&byline=0&title=0";
+    @endphp
 
     <div id="backgroundVideoContainer">
-      <video preload="none" playsinline autoplay muted loop id="">
-        <source src="{{asset('video/LANDING_2020.mp4')}}" type="video/mp4">
-        </video>
+      {{-- <video preload="none" playsinline autoplay muted loop id="">
+        <source src="{{$video}}" type="video/mp4">
+        </video> --}}
+        {{-- <iframe src="{{$video}}" width="" height=""></iframe> --}}
       </div>
 
+      <div class="vimeo-wrapper">
+         <iframe src="{{$video}}"
+                 frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+
+      {{-- <div class="crossfade">
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      </div> --}}
 
   </section>
-
-  {{-- <div class="crossfade">
-  <figure></figure>
-  <figure></figure>
-  <figure></figure>
-  <figure></figure>
-  <figure></figure>
-  <figure></figure>
-  <figure></figure>
-</div> --}}
 
   <section id="reel-section">
     @include('components.movies')
